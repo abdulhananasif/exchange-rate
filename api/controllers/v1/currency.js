b8a022f56510905d2ed96e5bbc8f897244940e84
Abdul Hanan Asif <99734167+abdulhananasif@users.noreply.github.com>
2023-08-09 23:20:04 +0500

    edit currency

--- api/controllers/v1/currency.js
+++ api/controllers/v1/currency.js
@@ -33,6 +33,44 @@ const createCurrency = async (req, res) => {
 
 const deleteCurrency = (req, res) => {};
 
-const editCurrency = async (req, res) => {};
+const editCurrency = async (req, res) => {
+  let response = {};
+  const schema = Joi.object({
+    currencyCode: Joi.string().required().messages({
+      'string.base': 'Currency code must be a string',
+      'any.required': 'Currency code is required',
+    }),
+    newExchangeRate: Joi.number().required().messages({
+      'number.base': 'New exchange Rate must be a number',
+      'any.required': 'New exchange Rate is required',
+    }),
+    newCurrencyCode: Joi.string().required().messages({
+      'string.base': 'New currency code must be a string',
+      'any.required': 'New currency code is required',
+    }),
+  });
+  try {
+    await schema.validateAsync(req.body);
+    const {currencyCode, newExchangeRate, newCurrencyCode} = req.body;
+    currencies = currencies.map((currency) => {
+      if (currencyCode === currency.currencyCode) {
+        currency.currencyCode = newCurrencyCode;
+        currency.exchangeRate = newExchangeRate;
+      }
+      return currency;
+    });
+
+    response.status = 200;
+    response.json = currencies;
+  } catch (err) {
+    let errorMessage = '';
+    err.details.forEach((error) => {
+      errorMessage += error.message;
+    });
+    response.status = 400;
+    response.json = {errorMessage};
+  }
+  res.status(response.status).json(response.json);
+};
 
 module.exports = {createCurrency, deleteCurrency, editCurrency};
