4573cb5792210db2bac4aced68d827db0d8ea661
Abdul Hanan Asif <99734167+abdulhananasif@users.noreply.github.com>
2023-08-09 22:45:18 +0500

    add currency api created

--- api/controllers/v1/currency.js
+++ api/controllers/v1/currency.js
@@ -1,11 +1,38 @@
+const Joi = require('joi');
+
 let {currencies} = require('../../commonVariables');
 
-const createCurrency = (req, res) => {
-  currencies.push({currencyCode: 'USD', exchangeRate: 305});
+const createCurrency = async (req, res) => {
+  let response = {};
+  const schema = Joi.object({
+    currencyCode: Joi.string().required().messages({
+      'string.base': 'Currency code must be a string',
+      'any.required': 'Currency code is required',
+    }),
+    exchangeRate: Joi.number().required().messages({
+      'number.base': 'Exchange Rate must be a number',
+      'any.required': 'Exchange Rate is required',
+    }),
+  });
+  try {
+    await schema.validateAsync(req.body);
+    const {currencyCode, exchangeRate} = req.body;
+    currencies.push({currencyCode, exchangeRate});
+    response.status = 201;
+    response.json = {currencies};
+  } catch (err) {
+    let errorMessage = '';
+    err.details.forEach((error) => {
+      errorMessage += error.message;
+    });
+    response.status = 400;
+    response.json = {errorMessage};
+  }
+  res.status(response.status).json(response.json);
 };
 
 const deleteCurrency = (req, res) => {};
 
-const editCurrency = (req, res) => {};
+const editCurrency = async (req, res) => {};
 
 module.exports = {createCurrency, deleteCurrency, editCurrency};
